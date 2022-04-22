567fd8319837f8af11657b70291171bd7ad8e9b2
varun <varunraj82786@gmail.com>
2022-04-22 21:27:05 +0530

    login updates

---
+++ spdashboard.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Service provider @ infinity</title>
+</head>
+
+<body>
+    <div>
+        <div>
+            <h2>Varun Anand</h2>
+            <div>
+                <p>
+                    <b>email</b><br><span>varunraj82786@gmail.com</span><br>
+                    <b>mobile no</b><br><span>9552726547</span>
+                </p>
+            </div>
+            <div>
+                <span>Service Avaiable :</span>
+                <div>
+                    <h3>Advocate</h3>
+                    <h4>Ahmednagar , Maharashtra</h4>
+                    <b>Time </b>
+                    <span>
+                        09:00:00 AM to 01:00:00 PM
+                    </span>
+                    <b>Service Charges</b>
+                    <span>
+                        399$
+                    </span>
+                </div>
+            </div>
+        </div>
+        <HR></HR>
+        <div>
+            <h2>Kushal Gohil</h2>
+            <div>
+                <p>
+                    <b>email</b><br><span>kushalgohil21@gmail.com</span><br>
+                    <b>mobile no</b><br><span>7040099240</span>
+                </p>
+            </div>
+            <div>
+                <span>Service Avaiable :</span>
+                <div>
+                    <h3>Software Engineer</h3>
+                    <h4>Pune,Maharashtra</h4>
+                    <b>Time </b>
+                    <span>
+                        02:00:00 AM to 08:00:00 AM
+                    </span>
+                    <b>Service Charges</b>
+                    <span>
+                        299$
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
