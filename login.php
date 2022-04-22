696d0b4d8483bf247ac3a60564b246477b68ba93
varun <varunraj82786@gmail.com>
2022-04-22 09:48:02 +0530

    updates 1.0

--- login.php
+++ login.php
@@ -12,9 +12,8 @@
   <title>Infinity Services Login</title>
     <link rel="icon" type="image/png" sizes="32x32" href="./Assets/Favicons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="./Assets/Favicons/favicon-16x16.png">
-  <title>Infinity Services Signup</title>
+  <title>Login @ Infinity</title>
 </head>
-
 <body>
   <section class="lsSection">
     <div class="lsModal boxShadow1">
