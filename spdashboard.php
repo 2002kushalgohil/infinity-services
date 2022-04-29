083d6e4e3af3963cd45a357463744eea5f4a6e14
Kushal Gohil <2002kushalgohil@gmail.com>
2022-04-29 19:03:49 +0530

    Made logic for updating and inserting spdata

--- spdashboard.php
+++ spdashboard.php
@@ -1,3 +1,68 @@
+<?php
+    //  ----------------- Include Connection file --------------------
+    include ("conn.php");
+
+    //  ----------------- Session start --------------------
+    session_start();
+    $userName = $_SESSION['sess_user'];
+    
+    // -------------------- Fetch user Id from dtabase --------------------
+    mysqli_select_db($conn, 'infinity') or die(mysqli_error($conn));
+    $userId = mysqli_query($conn, "SELECT id FROM Login WHERE user_id= '$userName'");
+    $id2=mysqli_fetch_assoc($userId);
+    $unFIlteredSPData = mysqli_query($conn, "SELECT * FROM sproviders WHERE id= '$id2[id]'");
+    $spData = mysqli_fetch_array($unFIlteredSPData);
+
+    // if $spData is empty the make it as an empty array
+    if(!$spData){
+        $spData['sname'] = "";
+        $spData['smail'] = "";
+        $spData['smob'] = "";
+        $spData['serv'] = "";
+        $spData['slocation'] = "";
+        $spData['stime'] = "";
+        $spData['etime'] = "";
+        $spData['scharges'] = "";
+    }
+
+
+
+    // ------------------ Sending Data to server as per condition --------------------
+    if(isset($_POST['add_prof'])){
+        $sname = $_POST['sname'];
+        $smail = $_POST['smail'];
+        $mobno = $_POST['mobno'];
+        $serv = $_POST['serv'];
+        $loc = $_POST['loc'];
+        $ftime = $_POST['ftime'];
+        $ttime = $_POST['ttime'];
+        $rate = $_POST['rate'];
+        if(empty($sname) || empty($smail) || empty($mobno) || empty($serv) || empty($loc) || empty($ftime) || empty($ttime) || empty($rate)){
+            echo "<script>alert('Please fill all the fields')</script>";
+        }
+        else{
+            if($spData['sname'] == ""){
+                $insertSP = mysqli_query($conn, "INSERT INTO sproviders (id, sname, smail, smob, serv, slocation, stime, etime, scharges) VALUES ('$id2[id]', '$sname', '$smail', '$mobno', '$serv', '$loc', '$ftime', '$ttime', '$rate')");
+                if($insertSP){
+                    echo "<script>alert('Profile Added Successfully')</script>";
+                    echo "<script>window.location.href='spdashboard.php'</script>";
+                }
+                else{
+                    echo "<script>alert('Profile Not Added')</script>";
+                }
+            }else{
+                $updateSP = mysqli_query($conn, "UPDATE sproviders SET sname='$sname', smail='$smail', smob='$mobno', serv='$serv', slocation='$loc', stime='$ftime', etime='$ttime', scharges='$rate' WHERE id='$id2[id]'");
+                if($updateSP){
+                    echo "<script>alert('Profile Updated Successfully')</script>";
+                    echo "<script>window.location.href='spdashboard.php'</script>";
+                }
+                else{
+                    echo "<script>alert('Profile Not Updated')</script>";
+                }
+            }
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -20,28 +85,28 @@
             <form action="" method="POST">
                 <fieldset>
                     <div><label for="sname">What is your full name ?</label>
-                        <input type="text" name="sname" id="sname">
+                        <input type="text" name="sname" id="sname" value="<?php echo"$spData[sname]" ?>">
                     </div>
                     <div>
                         <label for="smail">enter Your email :</label>
-                        <input type="email" name="smail" id="smail">
+                        <input type="email" name="smail" id="smail"value="<?php echo"$spData[smail]" ?>">
                     </div>
                     <div> <label for="mobno">Enter Your Mobile :</label>
-                        <input type="tel" name="mobno" id="mobno">
+                        <input type="tel" name="mobno" id="mobno"value="<?php echo"$spData[smob]" ?>">
                     </div>
                     <div> <label for="serv">Which Service Do You Provide ?</label>
-                        <input type="text" name="serv" id="serv">
+                        <input type="text" name="serv" id="serv"value="<?php echo"$spData[serv]" ?>">
                     </div>
                     <div> <label for="loc">From Where Do You Serve ?</label>
-                        <input type="text" name="loc" id="loc">
+                        <input type="text" name="loc" id="loc"value="<?php echo"$spData[slocation]" ?>">
                     </div>
                     <div> <label for="time">May I know Your Time of available ?</label>
-                        <input type="time" name=ftime id="time">
-                        <input type="time" name=ttime id="time">
+                        <input type="time" name=ftime id="time"value="<?php echo"$spData[stime]" ?>">
+                        <input type="time" name=ttime id="time"value="<?php echo"$spData[etime]" ?>">
                     </div>
                     <div>
                         <label for="rate">What's Your Service Charges ?</label>
-                        <input type="number" name="rate" id="rate">
+                        <input type="number" name="rate" id="rate"value="<?php echo"$spData[scharges]" ?>">
                     </div>
                     <div>
                         <button type="submit" name="add_prof">Add Profile</button>
@@ -51,58 +116,10 @@
             </form>
         </div>
     </div>
-    <?php
-     include('conn.php');
-     session_start();
-     $usr = $_SESSION['sess_user'];
+    
 
-     mysqli_select_db($conn, 'infinity') or die(mysqli_error($conn));
-     $id = mysqli_query($conn, "SELECT id FROM login WHERE user_id='$usr'");
-    if (isset($_POST['add_prof'])) {
-     $sname = $_POST['sname'];
-     $smail = $_POST['smail'];
-     $mobno = $_POST['mobno'];
-     $serv = $_POST['serv'];
-     $loc = $_POST['loc'];
-     $ftime = $_POST['ftime'];
-     $ttime = $_POST['ttime'];
-     $rate = $_POST['rate'];
-        if (!empty($_POST['sname']) && !empty($_POST['smail']) && !empty($_POST['mobno']) && !empty($_POST['serv']) && !empty($_POST['loc']) && !empty($_POST['ftime']) && !empty($_POST['ttime']) && !empty($_POST['rate'])) {
-           
-            $idrows = mysqli_num_rows($id);
-            if ($idrows != 0) {
-                while ($id2 = mysqli_fetch_assoc($id)) {
-                    mysqli_select_db($conn, 'infinity') or die(mysqli_error($conn));
-                    $sprof = mysqli_query($conn, "INSERT INTO sproviders (id,sname,smail,smob,service,slocation,stime,etime,scharges) VALUES ('$id2[0]','$sname','$smail','$mobno','$serv','$loc','$ftime','$ttime','$rate')");
-                }
-            }
-            if ($sprof) {
-                echo '<script>';
-                echo 'alert("Profile Added !")';
-                echo '</script>';
-            }
-        } else {
-            echo "<script>confirm('all fields are mandatory !')</script>";
-        }
-    }
-
-    $id2=mysqli_fetch_assoc($id);
-    $sp_data = mysqli_query($conn, "SELECT * FROM sproviders WHERE id=$id2[id]");
-    $sprows = mysqli_num_rows($sp_data);
-    if ($sprows != 0) {
-        while ($row = mysqli_fetch_assoc($sp_data)) {
-            echo "SP Id :$row[id] <br>";
-            echo "Full Name :$row[sname]<br>";
-            echo " Email ID :$row[smail] <br>";
-            echo " Mobile No :$row[smob] <br>";
-            echo "Service :$row[serv] <br>";
-            echo "At Location :$row[slocation] <br>";
-            echo "Available From $row[stime]";
-            echo " To :$row[etime]<br>";
-            echo "Service Charges :$row[scharges]";
-        }
-    }
-
+    <?php 
+        
     ?>
 </body>
 
