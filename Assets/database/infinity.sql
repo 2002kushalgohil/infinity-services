5a68f7c3869c97838d785882bf356a477aba4d59
Kushal Gohil <2002kushalgohil@gmail.com>
2022-05-06 18:43:16 +0530

    added data in database

--- Assets/database/infinity.sql
+++ Assets/database/infinity.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 05, 2022 at 07:54 PM
+-- Generation Time: May 06, 2022 at 03:10 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.4
 
@@ -39,25 +39,23 @@ CREATE TABLE `login` (
 --
 
 INSERT INTO `login` (`id`, `user_id`, `password`, `stype`) VALUES
-(143, 'abdul', '11', 'sp'),
-(140, 'abhi', '11', 'sp'),
-(146, 'aishwarya', '11', 'sp'),
-(150, 'anamika', '11', 'user'),
-(158, 'demo', '11', 'sp'),
-(141, 'dev', '11', 'sp'),
-(151, 'gopal', '11', 'user'),
-(144, 'john', '11', 'user'),
-(145, 'karishma', '11', 'sp'),
-(147, 'krish', '11', 'sp'),
-(157, 'kushal', '11', 'admin'),
-(152, 'madhav', '11', 'user'),
-(149, 'pinky', '11', 'sp'),
-(139, 'raj', '11', 'user'),
-(142, 'rocky', '11', 'sp'),
-(154, 'rudisn', '11', 'user'),
-(156, 'ss', '11', 'sp'),
-(138, 'varun', '11', 'sp'),
-(148, 'vikram', '11', 'sp');
+(169, 'anita', '11', 'sp'),
+(162, 'drake', '11', 'user'),
+(163, 'eminem', '11', 'user'),
+(174, 'gita', '11', 'sp'),
+(164, 'jennifer', '11', 'user'),
+(160, 'john', '11', 'user'),
+(159, 'kushal', '11', 'admin'),
+(166, 'mohammed', '11', 'sp'),
+(173, 'rajesh', '11', 'sp'),
+(165, 'ram', '11', 'sp'),
+(175, 'ramesh', '11', 'sp'),
+(171, 'sanjay', '11', 'sp'),
+(170, 'santosh', '11', 'sp'),
+(168, 'sri', '11', 'sp'),
+(172, 'sunil', '11', 'sp'),
+(167, 'sunita', '11', 'sp'),
+(161, 'travis', '11', 'user');
 
 -- --------------------------------------------------------
 
@@ -84,12 +82,13 @@ CREATE TABLE `services` (
 --
 
 INSERT INTO `services` (`serv_id`, `usr_id`, `sp_id`, `sname`, `serv`, `uname`, `location`, `serv_status`, `ratings`, `rdate`, `rtime`) VALUES
-(124, 144, 143, 'Abdul karim', 'Mechanic', 'John yacruise', 'pune', 'Paid', 3, '2022-05-05', '00:00:00'),
-(125, 150, 140, 'Abhinav bankar', 'Mechanic', 'Anamika khanna', 'pune', 'Paid', 2, '2022-05-12', '18:14:00'),
-(126, 151, 141, 'Dev varma', 'Pet sitting', 'Gopal dhamaal', 'mumbai', 'UnPaid', 0, '2022-05-05', '18:46:00'),
-(127, 151, 138, 'Varun anand', 'Advocate', 'Gopal dhamaal', 'mumbai', 'Requested', 0, '2022-05-05', '20:47:00'),
-(128, 154, 148, 'Vikram bajaj', 'Advocate', 'Rudisn', 'Shr', 'Requested', 0, '2022-05-05', '18:46:00'),
-(129, 151, 145, 'Karishma patil', 'Makeup artist', 'Gopal dhamaal', 'mumbai', 'Requested', 0, '2022-05-20', '22:26:00');
+(130, 162, 169, 'Anita Garcia', 'Fitness trainer', 'Drake Laghari', 'Hyderabad', 'Paid', 4, '2022-05-06', '18:58:00'),
+(131, 162, 166, 'Mohammed Johnson', 'Mechanic', 'Drake Laghari', 'Hyderabad', 'UnPaid', 1, '2022-05-10', '10:20:00'),
+(132, 163, 173, 'Rajesh Lopez', 'Carpenter', 'Eminem Agarwal', 'Meerut', 'Paid', 3, '2022-05-08', '18:31:00'),
+(133, 164, 167, 'Sunita Williams', 'Electrician', 'Jennifer Reddy', 'Bangalore', 'Rejected', 0, '2022-05-27', '09:27:00'),
+(134, 160, 171, 'Sanjay Davis', 'Pet sitting', 'John balakrishnan', 'Shrirampur', 'UnPaid', 3, '2022-05-28', '21:37:00'),
+(135, 162, 174, 'Gita Wilson', 'Plumber', 'Drake Laghari', 'Hyderabad', 'Paid', 3, '2022-05-19', '18:39:00'),
+(136, 162, 171, 'Sanjay Davis', 'Pet sitting', 'Drake Laghari', 'Hyderabad', 'Finished', 0, '2022-05-18', '14:26:00');
 
 -- --------------------------------------------------------
 
@@ -115,18 +114,17 @@ CREATE TABLE `sproviders` (
 --
 
 INSERT INTO `sproviders` (`id`, `sname`, `smail`, `smob`, `serv`, `slocation`, `stime`, `etime`, `scharges`, `sdesc`) VALUES
-(156, 'ss', 'a@a.com', 8888888888, 'Doctor', 'Delhi', '09:18:00', '09:17:00', 999, 'dsdsd'),
-(143, 'Abdul karim', 'abd360@yahoo.com', 9154654798, 'Mechanic', 'Shrirampur', '09:00:00', '21:00:00', 15, 'I am professional mechanic with expertise in heavy vehical auto works'),
-(140, 'Abhinav bankar', 'abhi777@gmail.com', 9156788654, 'Mechanic', 'Pune', '09:00:00', '16:00:00', 350, 'An experienced car mechanic at BMW'),
-(146, 'Aishwarya kapoor', 'aish784@gmail.com', 7850431254, 'Makeup artist', 'Mumbai', '10:00:00', '15:00:00', 150, 'an professional makeup artist'),
-(158, 'Demo name', 'd@d.com', 8888888888, 'Fitness trainer', 'Pune', '14:41:00', '14:41:00', 55, 'hjasdjhadsjygasdjygasdjygdjgyasfddfja'),
-(141, 'Dev varma', 'dev@gmail.com', 9158848876, 'Pet sitting', 'Shrirampur', '09:00:00', '17:00:00', 60, 'an animal lover and pet sitter'),
-(145, 'Karishma patil', 'kpatil@yahoo.com', 9531476454, 'Makeup artist', 'Baramati', '11:00:00', '16:00:00', 150, 'a professional makeup artist and ex-makeup artist of rk studios'),
-(147, 'Krishna chakraborthy', 'krish267@gmail.com', 9164578542, 'Electrician', 'Shrirampur', '08:00:00', '20:00:00', 45, 'An IIT graduate professional in electricals with experience of 3 years'),
-(149, 'Pinky m malhotra', 'pinky@gmail.com', 7040023999, 'Doctor', 'Pune', '09:00:00', '19:00:00', 40, 'an M.D. in orthopedics from british university london'),
-(138, 'Varun anand', 'raj343@gmail.com', 9552726547, 'Advocate', 'Shrirampur', '09:00:00', '13:30:00', 340, 'an experienced advocate in prominent of civil cases'),
-(142, 'Rocky singhania', 'rocks@rohit.com', 9646751414, 'Fitness trainer', 'Mumbai', '07:00:00', '09:30:00', 25, 'an owner and trainer of fast & furious gym '),
-(148, 'Vikram bajaj', 'vbajaj12@gmail.com', 9154778985, 'Advocate', 'Nagpur', '10:00:00', '14:00:00', 450, 'civil lawyer as a consultant to MNCs');
+(169, 'Anita Garcia', 'anitagarcia76@gmail.com', 5452102369, 'Fitness trainer', 'Meerut', '05:00:00', '12:00:00', 300, 'As a personal trainer, lead individualized workouts incorporating aerobic and anaerobic exercises. Work with clients to evaluate fitness levels, set goals, monitor progress and blast through plateaus. As a group fitness instructor, lead fun, energetic classes for diverse age groups and fitness levels.'),
+(174, 'Gita Wilson', 'gitawilson73@gmail.com', 5210000089, 'Plumber', 'Varanasi', '10:20:00', '22:20:00', 200, 'Install, repair, and maintain pipes, valves, fittings, drainage systems, and fixtures in commercial and residential structures.'),
+(166, 'Mohammed Johnson', 'mohammedjohnson99@gmail.com', 8865212013, 'Mechanic', 'Delhi', '10:00:00', '22:00:00', 150, 'Highly skilled and dependable with hands-on experience and excellent training to perform service, diagnostics, and repairs on domestic and imported automobiles and trucks.'),
+(173, 'Rajesh Lopez', 'rajeshlopez990@gmail.com', 5652011123, 'Carpenter', 'Pune', '09:05:00', '22:25:00', 200, 'Automotive Painters perform repairs to vehicles which have usually been damaged in a collision. Usual work activities listed on an Automotive Painter resume sample are removing rust, filling cavities, masking details, picking the right colors, using paint sprayers, and checking quality when the job is finished.'),
+(175, 'Ramesh Anderson', 'Rameshanderson89@gmail.com', 5566889950, 'Photographer', 'Agra', '20:15:00', '18:27:00', 883, 'discussing requirements with clients, performing research for each shoot, using technical equipment, networking with other professionals, arranging photo shoot backgrounds, processing images, and self-marketing.'),
+(165, 'Ram Smith', 'Ramsmith88@gmail.com', 7788996520, 'Advocate', 'Shrirampur', '09:00:00', '20:00:00', 889, 'Hands-on experience in maintaining knowledge of the available resources. Immense knowledge of legal and criminal systems. Excellent knowledge of battling various women issues. Ability to maintain policies for all development issues.'),
+(171, 'Sanjay Davis', 'sanjaydavis46@gmail.com', 3322665540, 'Pet sitting', 'Chennai', '10:15:00', '18:15:00', 175, 'Responsible of checking on clients pets and assuring that their necessities were being met.'),
+(170, 'Santosh Miller', 'santoshmiller90@gmail.com', 7789554630, 'Makeup artist', 'Mumbai', '09:00:00', '22:00:00', 400, 'Highly dedicated and skilled Makeup Artist with an exceptional record of customer service and client satisfaction. Adept at perceiving individual clients’ unique skin tone and corresponding makeup needs and recommending products accordingly. Abel to work well independently or as part of a cosmetology professional team.'),
+(168, 'Sri Jones', 'srijones67@gmail.com', 9632545698, 'Doctor', 'Hyderabad', '08:00:00', '22:00:00', 899, 'Responsible physician with 9 years of experience maximizing patient wellness and facility profitability. Seeking to deliver healthcare excellence at Mercy Hospital. At CRMC, maintained 5-star healthgrades score for 112 reviews and 85% patient success.'),
+(172, 'Sunil Rodriguez', 'sunilrodriguez89@gmail.com', 5212021350, 'Ac Repairing', 'Kolkata', '10:00:00', '20:20:00', 450, 'In-depth Knowledge and experience to install, maintain and repair AC. Great knowledge of the laws, regulations and safety procedures related to AC equipment. Operational knowledge of electrical circuits and schematics'),
+(167, 'Sunita Williams', 'sunitawilliams100@gmail.com', 5265878950, 'Electrician', 'Madras', '08:00:00', '18:00:00', 200, 'Skilled journeyman electrician with 7+ years of experience installing, repairing, and maintaining low-voltage electrical systems. ');
 
 -- --------------------------------------------------------
 
@@ -147,12 +145,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `uname`, `umail`, `umob`, `location`) VALUES
-(150, 'Anamika khanna', 'anu@anu.com', 9145247264, 'Pune'),
-(151, 'Gopal dhamaal', 'gopueda@rohit.com', 7464199541, 'Mumbai'),
-(144, 'John yacruise', 'johny24@hotmail.com', 9169368420, 'Pune'),
-(152, 'Madhav dhamaal', 'madhu@rohit.com', 9856318979, 'Mumbai'),
-(139, 'Raj malhotra', 'raj123@gmail.com', 9152014365, 'Shrirampur'),
-(154, 'Rudisn', 'k@k.com', 8888888888, 'Shrirampur');
+(162, 'Drake Laghari', 'drakelaghari66@gmail.com', 7788994455, 'Hyderabad'),
+(163, 'Eminem Agarwal', 'eminemagarwal@gmail.com', 8978456582, 'Meerut'),
+(164, 'Jennifer Reddy', 'jenniferreddy33@gmail.com', 5566448877, 'Bangalore'),
+(160, 'John balakrishnan', 'johnbalakrishnan1@gmail.com', 7894561237, 'Shrirampur'),
+(161, 'Travis Iyer', 'travisIyer69@gmail.com', 4567891230, 'Delhi');
 
 --
 -- Indexes for dumped tables
@@ -195,13 +192,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=159;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=176;
 
 --
 -- AUTO_INCREMENT for table `services`
 --
 ALTER TABLE `services`
-  MODIFY `serv_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=130;
+  MODIFY `serv_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=137;
 
 --
 -- Constraints for dumped tables
